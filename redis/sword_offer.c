1cdaf3f17c11ac48f8e2930649cf1b848090e755
wangsen1994 <990660523@qq.com>
2019-07-14 19:12:46 +0800

    add

--- redis/sword_offer.c
+++ redis/sword_offer.c
@@ -902,3 +902,24 @@ bool duplicate(int numbers[], int length, int* duplication) {
         }
         return false;
     }
+
+# 51 构建乘积数组
+vector<int> multiply(const vector<int>& A) {
+
+	int n=A.size();
+    vector<int> b(n);
+    int ret=1;
+    for(int i=0;i<n;ret*=A[i++]){
+        b[i]=ret;
+    }
+    ret=1;
+    for(int i=n-1;i>=0;ret*=A[i--]){
+        b[i]*=ret;
+    }
+    return b;
+}
+
+# 52 正则表达式匹配
+bool match(char* str, char* pattern){
+    
+    }
